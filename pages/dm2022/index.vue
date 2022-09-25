64c64761d91be4685cdee74df4da779dff2bd564
HumbleCatcher <andreas.ellison@gmail.com>
2022-09-25 21:44:10 +0200

    update information for first exercise class

---
+++ pages/dm2022/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <default-layout title="Diskrete Mathematik HS22" back="/">
+    <h3>Termine</h3>
+    <h4>Vorlesung</h4>
+    Montag / Mittwoch — 14-16 —
+    <a
+      target="_blank"
+      href="https://www.rauminfo.ethz.ch/Rauminfo/grundrissplan.gif?gebaeude=CAB&geschoss=G&raumNr=56&lang=de"
+    >
+      ETA F 5
+    </a>
+    <h4>Übungsstunde</h4>
+    Montag — 16-18 —
+    <a
+      target="_blank"
+      href="http://www.rauminfo.ethz.ch/Rauminfo/grundrissplan.gif?gebaeude=CAB&geschoss=G&raumNr=56&lang=de"
+    >
+      CAB G 56
+    </a>
+
+    <h3>Kontakt</h3>
+    <ul>
+      <li>Je m'appelle Andreas Ellison</li>
+      <li>
+        Email:
+        <a href="mailto:aellison@student.ethz.ch">aellison@student.ethz.ch</a>
+      </li>
+      <li>
+        Discord: HumbleCatcher#1527 <br />
+        (I prefer this over email, don't hesitate to ask me questions 🙂)
+      </li>
+    </ul>
+
+    <h3>Links</h3>
+    <ul>
+      <li>
+        <a target="_blank" href="https://crypto.ethz.ch/teaching/DM22/"
+          >Vorlesungswebseite</a
+        >
+      </li>
+      <li>
+        <a
+          target="_blank"
+          href="https://crypto.ethz.ch/teaching/DM22/ln/DM22_LN_tablet_z21nvff0ch2gj1ol0mt1.pdf"
+          >le Skript</a
+        >
+      </li>
+      <li>
+        <a target="_blank" href="https://dm.crypto.ethz.ch/"> Übungsaufgaben</a>
+      </li>
+    </ul>
+
+    <h3>Übungsstunden</h3>
+    <ul>
+      <li>
+        <span class="outer">
+          <NuxtLink to="/dm2022/week/1"> Woche 1 </NuxtLink>
+        </span>
+      </li>
+    </ul>
+  </default-layout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="sass" scoped></style>
---
+++ pages/dm2022/week/1.vue
@@ -0,0 +1,38 @@
+<template>
+  <default-layout title="Diskrete Mathematik HS22 - Woche 1" back="/dm2022">
+    <h3>Übersicht</h3>
+    <ul>
+      <li>Einführung</li>
+      <li>Besprechung Aufgaben (1.1 und) 1.2 von der Serie</li>
+      <li>Logik: Beispiele, Repetition und Aufgaben</li>
+      <li>Hilbert's Hotel (falls Zeit übrig)</li>
+    </ul>
+    <h3>Notizen</h3>
+    <i
+      >Ich werde (versuchen) meine Notizen aus der Übungsstunde spätestens am
+      26.9. abends da hochzuladen.</i
+    >
+    <h3>Ressourcen</h3>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          href="https://polybox.ethz.ch/index.php/s/WXf1p3ODpDdpnRH?path=%2F1.%20Semester%2F2%20DiskMath%2F3%20Exercises%2FHS20"
+          >Polybox</a
+        >
+        - Hilbert's Hotel (u01.pdf)
+      </li>
+      <li><nuxt-link to="/logic-tools"> Kleine Tools für Logik </nuxt-link></li>
+    </ul>
+  </default-layout>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  mounted() {},
+});
+</script>
+
+<style lang="sass" scoped></style>
