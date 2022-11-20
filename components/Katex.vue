4307805e55fed33ea93b49b45d94eedbf908f2e2
HumbleCatcher <andreas.ellison@gmail.com>
2022-11-20 18:32:47 +0100

    add Katex component as a convenient wrapper for using latex

---
+++ components/Katex.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    v-html="$katex(t)"
+    :class="{
+      'py-4': block,
+      'text-center': block,
+      'd-inline': !block,
+    }"
+  />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    t: {
+      type: String,
+      required: true,
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
