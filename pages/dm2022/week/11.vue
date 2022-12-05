3112076603c7b2267d805e27c28d596b01277a6d
HumbleCatcher <andreas.ellison@gmail.com>
2022-12-05 15:59:03 +0100

    upload content for week 11

---
+++ pages/dm2022/week/11.vue
@@ -0,0 +1,18 @@
+<template>
+  <default-layout title="Diskrete Mathematik HS22 - Woche 11" back="/dm2022">
+    <h3>Übersicht</h3>
+    <ul>
+      <li>Feedback</li>
+      <li>Beweisaufgabe mit Polynomen</li>
+      <li>Proof systems (Aufgabe)</li>
+      <li>Kalküle (Aufgabe)</li>
+    </ul>
+    <h3>Links</h3>
+    <ul>
+      <li>
+        <a :href="$static('/dm2022/week11/notes.pdf')">Meine Notizen</a> und
+        <a :href="$static('/dm2022/week11/slides.pdf')">die Slides</a>
+      </li>
+    </ul>
+  </default-layout>
+</template>
