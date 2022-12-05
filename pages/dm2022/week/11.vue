e7472ace44291fa5823ff37d9503784d723ce209
HumbleCatcher <andreas.ellison@gmail.com>
2022-12-05 19:35:24 +0100

    add recommendation for exercises

--- pages/dm2022/week/11.vue
+++ pages/dm2022/week/11.vue
@@ -8,6 +8,12 @@
       <li>Kalküle (Aufgabe)</li>
     </ul>
     <h3>Links</h3>
+    <h3>Priorität bei den Aufgaben</h3>
+    Aufgaben 2 (sehr ähnlich Aufgabe in meinen Notizen), 6, 7 sind
+    Standardaufgaben, sollte man auf jeden Fall lösen. <br />
+    Die anderen Aufgaben sind sehr gut, um die Konzepte von der Vorlesung besser
+    zu verstehen. Es lohnt sich ebenfalls sehr, sich damit zu befassen.
+    <h3>Links</h3>
     <ul>
       <li>
         <a :href="$static('/dm2022/week11/notes.pdf')">Meine Notizen</a> und
@@ -16,7 +22,7 @@
       <li>
         Beispiel
         <a :href="$static('/dm2022/week11/calculation.py')">Script</a> um mit
-        Polynomen in Python zu rechnen
+        Polynomen zu rechnen in Python
       </li>
       <li>
         <a href="https://forms.gle/AYJmwPzzcWRLeBye7">Formular</a> wegen dem
